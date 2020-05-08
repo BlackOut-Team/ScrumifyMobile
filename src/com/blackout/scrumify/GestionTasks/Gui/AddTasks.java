eebf075e66fd96007fccbf316278754e0cfd8e16
hidaya <hidaya.mcharek@esprit.tn>
2020-05-08 22:41:35 +0200

    crud part 1
    if something changed or didnt work try to check statics and scrumify.class

---
+++ src/com/blackout/scrumify/GestionTasks/Gui/AddTasks.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blackout.scrumify.GestionTasks.Gui;
+
+import com.codename1.ui.util.Resources;
+
+/**
+ *
+ * @author Hidaya
+ */
+public class AddTasks {
+
+   
+    
+}
